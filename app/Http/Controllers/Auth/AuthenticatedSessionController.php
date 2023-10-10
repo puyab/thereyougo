8b9dec5732abac53e2147c3ff81a619fe74a827d
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-10-10 13:39:53 +0200

    Optimized the auth routes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use ProtoneMedia\Splade\Facades\Toast;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -30,8 +31,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME);
+        Toast::success('You logged in')->leftBottom();
+        return redirect(route('home'));
     }
 
     /**
