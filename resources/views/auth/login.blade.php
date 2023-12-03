3dbb55b0d7fc306abfe9a9b2c49e49c07d79078a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-12-03 18:58:05 +0330

    Added the ResetPassword link to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,23 @@
-{{--<x-guest-layout>--}}
-{{--    <x-auth-card>--}}
-{{--        <!-- Session Status -->--}}
-{{--        <x-auth-session-status class="mb-4" />--}}
+{{-- <x-guest-layout> --}}
+{{--    <x-auth-card> --}}
+{{--        <!-- Session Status --> --}}
+{{--        <x-auth-session-status class="mb-4" /> --}}
 
 {{--
-{{--    </x-auth-card>--}}
-{{--</x-guest-layout>--}}
+{{--    </x-auth-card> --}}
+{{-- </x-guest-layout> --}}
 
 <x-layouts.auth type="login">
-        <x-splade-form action="{{ route('login') }}"
-                       class="w-full h-max flex flex-col gap-4 items-center">
-            <x-input id="email" type="email" name="email" v-model="form.email" placeholder="Email" required autofocus/>
-            <x-input id="password" type="password" name="password" v-model="form.password" placeholder="Password" required
-                     autocomplete="current-password"/>
-            <x-button class="mt-16 lg:mt-36">
+    <x-splade-form action="{{ route('login') }}" class="w-full h-max flex flex-col gap-4 items-center">
+        <x-input id="email" type="email" name="email" v-model="form.email" placeholder="Email" required autofocus />
+        <x-input id="password" type="password" name="password" v-model="form.password" placeholder="Password" required
+            autocomplete="current-password" />
+        <div class="mt-16 lg:mt-36 flex flex-row items-center justify-center gap-6">
+            <x-button>
                 Continue
             </x-button>
-        </x-splade-form>
+            <Link class="text-2xl font-medium underline" href="{{ route('reset-password') }}">Reset Password</Link>
+        </div>
+
+    </x-splade-form>
 </x-layouts.auth>
