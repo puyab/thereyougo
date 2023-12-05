50a1053bb9b6625873f95641f0a8837ff722dd7a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-12-05 17:27:13 +0330

    Fixed the ResetPassword font size

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
   <x-splade-form action="{{ route('login') }}" class="w-full h-max flex flex-col gap-4 items-center">
     <x-input id="email" type="email" name="email" v-model="form.email" placeholder="Email" required autofocus />
     <x-input id="password" type="password" name="password" v-model="form.password" placeholder="Password" required autocomplete="current-password" />
-    <Link class="text-2xl font-medium underline mr-auto mt-2 lg:mt-4" href="{{ route('reset-password') }}">Reset Password</Link>
+    <Link class="text-xl font-medium underline mr-auto mt-2 lg:mt-4" href="{{ route('reset-password') }}">Reset Password</Link>
     <x-button class="mt-16 lg:mt-36">
       Continue
     </x-button>
