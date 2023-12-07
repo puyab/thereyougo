59643a02f83c4767a23943fd62c26a946ce94e00
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-12-07 16:40:06 +0330

    Fixed the font

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,15 @@
 <x-layouts.auth type="login">
-  <x-splade-form action="{{ route('login') }}" class="w-full h-max flex flex-col gap-4 items-center">
-    <x-input id="email" type="email" name="email" v-model="form.email" placeholder="Email" required autofocus />
-    <x-input id="password" type="password" name="password" v-model="form.password" placeholder="Password" required autocomplete="current-password" />
-    <div class="w-full h-10 flex items-center justify-start pl-3">
-      <Link class="text-xl font-inter font-medium underline mt-2 lg:mt-4" href="{{ route('reset-password') }}">Reset Password</Link>
-    </div>
-    <x-button class="mt-16 lg:mt-32">
-      Continue
-    </x-button>
+    <x-splade-form action="{{ route('login') }}" class="w-full h-max flex flex-col gap-4 items-center">
+        <x-input id="email" type="email" name="email" v-model="form.email" placeholder="Email" required autofocus />
+        <x-input id="password" type="password" name="password" v-model="form.password" placeholder="Password" required
+            autocomplete="current-password" />
+        <div class="w-full h-10 flex items-center justify-start pl-3">
+            <Link class="text-lg font-inter font-medium underline mt-2 lg:mt-4" href="{{ route('reset-password') }}">
+            Reset Password</Link>
+        </div>
+        <x-button class="mt-16 lg:mt-32">
+            Continue
+        </x-button>
 
-  </x-splade-form>
+    </x-splade-form>
 </x-layouts.auth>
