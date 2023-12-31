2fcc132f6f00abfdf2fc89de468edb43679510b6
naser <na3r.jafari@gmail.com>
2023-12-31 16:04:51 +0330

    a12

---
+++ resources/views/components/home/peers-slider.blade.php
@@ -0,0 +1,60 @@
+@props(['profiles'])
+@php
+    $breakpoints = [
+        0 => [
+            'slidesPerView' => 2,
+        ],
+        420 => [
+            'slidesPerView' => 2.3,
+            'spaceBetween' => 38,
+        ],
+        600 => [
+            'centerMode' => true,
+            'slidesPerView' => 3,
+            'spaceBetween' => 30,
+        ],
+        900 => [
+            'slidesPerView' => 4,
+            'spaceBetween' => 50,
+        ],
+        1024 => [
+            'slidesPerView' => 2.5,
+            'spaceBetween' => 72,
+        ],
+        1200 => [
+            'slidesPerView' => 3,
+            'spaceBetween' => 72,
+        ],
+        1500 => [
+            'slidesPerView' => 4,
+            'spaceBetween' => 72,
+        ],
+        1700 => [
+            'slidesPerView' => 4.3,
+            'spaceBetween' => 72,
+        ],
+        1840 => [
+            'slidesPerView' => 5,
+            'spaceBetween' => 72,
+        ],
+    ];
+@endphp
+<Swiper class="w-full mx-auto max-w-[1920px]" :spaceBetween="24" :loop="true" :autoplay="@js(['delay' => rand(4000, 8000)])"
+    : v-bind:modules="SwiperModules" :breakpoints="@js($breakpoints)">
+        <SwiperSlide >
+          <a class="logo-item" href="#"><img src="images/logo1.png" alt=""></a>
+        </SwiperSlide>
+        <SwiperSlide >
+          <a class="logo-item" href="#"><img src="images/logo2.png" alt=""></a>
+        </SwiperSlide>
+        <SwiperSlide >
+          <a class="logo-item" href="#"><img src="images/logo3.png" alt=""></a>
+        </SwiperSlide>
+        <SwiperSlide >
+          <a class="logo-item" href="#"><img src="images/logo4.png" alt=""></a>
+        </SwiperSlide>
+        <SwiperSlide >
+          <a class="logo-item" href="#"><img src="images/logo5.png" alt=""></a>
+        </SwiperSlide>
+    
+</Swiper>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,27 +1,57 @@
-<nav class="w-full h-max bg-[#D29A9A80] flex items-center justify-between px-6 py-4 lg:py-9 lg:px-20">
-    <Link href="{{ route('home') }}">
-    <figure>
-        <img src="{{ asset('images/logo.png') }}" alt="Logo"
-            class="w-11 lg:w-full max-w-[86.73px] aspect-auto opacity-50" />
-    </figure>
-    </Link>
-    <div class="w-max h-max flex items-center justify-center gap-7 lg:gap-14">
-        @if (auth()->check())
-            <x-link class="font-bold text-xs sm:text-base text-black font-inter"
-                href="{{ route('logout') }}">Log-out</x-link>
-            <Link href="{{ route('profile') }}" class="font-bold text-xs sm:text-base text-black font-inter">Settings
-            </Link>
-            <Link href="{{ auth()->user()->profile->canAccessProfile()? route('profile.global'): '#' }}">
-            <x-feathericon-user class="w-8 h-8 lg:w-12 lg:h-12 text-white bg-black rounded-full p-2" />
-            </Link>
-        @else
-            <a href="#faqs" class="font-bold text-xs sm:text-base text-black hidden md:block font-inter">FAQs</a>
-            <x-link :href="route('login')" class="font-bold text-xs sm:text-base text-black font-inter">Login</x-link>
-            <x-link :href="route('register')">
-                <x-button>Apply
-                    now
-                </x-button>
-            </x-link>
-        @endif
+<div id="top-nav">
+    <div id="logo">
+        <a href="https://thereyougo.eu">
+            <img src="images/logo.png" alt="Logo">
+        </a>
     </div>
-</nav>
+  <nav>
+    <div class="w-max h-max flex items-center justify-center gap-7 lg:gap-14">
+      @if (auth()->check())
+          <x-link class="font-bold text-xs sm:text-base text-black font-inter"
+              href="{{ route('logout') }}">Log-out</x-link>
+          <Link href="{{ route('profile') }}" class="font-bold text-xs sm:text-base text-black font-inter">Settings
+          </Link>
+          <Link href="{{ auth()->user()->profile->canAccessProfile()? route('profile.global'): '#' }}">
+          <x-feathericon-user class="w-8 h-8 lg:w-12 lg:h-12 text-white bg-black rounded-full p-2" />
+          </Link>
+      @else
+          <a href="#faqs" class="font-bold text-xs sm:text-base text-black hidden md:block font-inter">FAQs</a>
+          <x-link :href="route('login')" class="font-bold text-xs sm:text-base text-black font-inter">Login</x-link>
+          <x-link :href="route('register')">
+              <x-button>Apply
+                  now
+              </x-button>
+          </x-link>
+      @endif
+  </div>
+  </nav>
+</div>
+
+
+{{--  <nav class="w-full h-max bg-[#D29A9A80] flex items-center justify-between px-6 py-4 lg:py-9 lg:px-20">
+  <Link href="{{ route('home') }}">
+  <figure>
+      <img src="{{ asset('images/logo.png') }}" alt="Logo"
+          class="w-11 lg:w-full max-w-[86.73px] aspect-auto opacity-50" />
+  </figure>
+  </Link>
+  <div class="w-max h-max flex items-center justify-center gap-7 lg:gap-14">
+      @if (auth()->check())
+          <x-link class="font-bold text-xs sm:text-base text-black font-inter"
+              href="{{ route('logout') }}">Log-out</x-link>
+          <Link href="{{ route('profile') }}" class="font-bold text-xs sm:text-base text-black font-inter">Settings
+          </Link>
+          <Link href="{{ auth()->user()->profile->canAccessProfile()? route('profile.global'): '#' }}">
+          <x-feathericon-user class="w-8 h-8 lg:w-12 lg:h-12 text-white bg-black rounded-full p-2" />
+          </Link>
+      @else
+          <a href="#faqs" class="font-bold text-xs sm:text-base text-black hidden md:block font-inter">FAQs</a>
+          <x-link :href="route('login')" class="font-bold text-xs sm:text-base text-black font-inter">Login</x-link>
+          <x-link :href="route('register')">
+              <x-button>Apply
+                  now
+              </x-button>
+          </x-link>
+      @endif
+  </div>
+</nav>  --}}
