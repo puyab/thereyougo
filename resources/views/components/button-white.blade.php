a0ec1a6600f59241b990f8c94e2911150cee7f2a
PUYA\puya2 <puya.bahrani@eprojectsrl.it>
2024-01-03 17:34:05 +0100

    text black

--- resources/views/components/button-white.blade.php
+++ resources/views/components/button-white.blade.php
@@ -1,3 +1,3 @@
-<button {{$attributes->merge(['class' => 'w-max px-3.5 py-2.5 md:px-[29px] md:py-3.5 bg-[#fff] text-white font-inter text-xs md:text-base font-semibold rounded-[4999px]'])}}>
+<button {{$attributes->merge(['class' => 'w-max px-3.5 py-2.5 md:px-[29px] md:py-3.5 bg-[#fff] text-black font-inter text-xs md:text-base font-semibold rounded-[4999px]'])}}>
     {{$slot}}
 </button>
