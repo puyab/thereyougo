53d5e6e072cd2f194f9d10e824ba3f7a9857a07a
PUYA\puya2 <puya.bahrani@eprojectsrl.it>
2024-01-03 17:15:22 +0100

    new logo desktop version

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div id="top-nav">
     <div id="logo">
         <Link href="{{ route('home') }}">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo">
+        <img src="images/logo_m.png" alt="Logo">
         </Link>
     </div>
     <nav>
