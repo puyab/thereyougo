86b08af64fcc45f022922c82d1d0228707a6853e
PUYA\puya2 <puya.bahrani@eprojectsrl.it>
2024-01-03 18:05:37 +0100

    fix font mobile version landing page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -101,7 +101,7 @@
 <section class="pb-4 pt-8 md:py-16 px-10 md:px-20 lg:px-24 text-center ">
     <Link href="{{ route('register') }}">
 
-    <p class="font-light text-[#292D32] block md:hidden">Worldwide, owners and renters welcome.</p>
+    <p class="text-[#292D32] block md:hidden">Worldwide, owners and renters welcome.</p>
     <br>
     <div class="buttons">
         <x-button class="lg:text-base max-h-[50px]">Apply now</x-button>
