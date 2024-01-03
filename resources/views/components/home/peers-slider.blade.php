89a0ffec4253cc439df99ae7620ce1e88f9b26b6
PUYA\puya2 <puya.bahrani@eprojectsrl.it>
2024-01-03 22:48:57 +0100

    removed the logo brand slider

--- resources/views/components/home/peers-slider.blade.php
+++ resources/views/components/home/peers-slider.blade.php
@@ -39,7 +39,7 @@
         ],
     ];
 @endphp
-<Swiper class="w-full mx-auto max-w-[1920px]" :spaceBetween="12" :loop="true" :autoplay="@js(['delay' => rand(4000, 8000)])"
+<!-- <Swiper class="w-full mx-auto max-w-[1920px]" :spaceBetween="6" :loop="true" :autoplay="@js(['delay' => rand(4000, 8000)])"
     :v-bind:modules="SwiperModules" :breakpoints="@js($breakpoints)" :speed="2000">
     <SwiperSlide>
         <a class="logo-item" href="#"><img src="images/logo1.png" alt=""></a>
@@ -57,4 +57,4 @@
         <a class="logo-item" href="#"><img src="images/logo5.png" alt=""></a>
     </SwiperSlide>
 
-</Swiper>
+</Swiper> -->
